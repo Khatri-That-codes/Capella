835b4f1353a94ee2d3c8134e9f5d8401a561581b
Jena Khatri <188423494+Khatri-That-codes@users.noreply.github.com>
2026-01-14 18:18:52 +1200

    Initial android files

---
+++ app/src/main/java/com/capella/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.capella
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.capella.ui.theme.CapellaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CapellaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    WelcomeScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun WelcomeScreen(modifier: Modifier = Modifier) {
+
+}
+
+
